b508f0d6c195a184c867eea58ace20d84df2aaec
carolgreene <carolgreene3@gmail.com>
2020-02-29 12:36:13 -0800

    working app except for decimals

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 //set variables needed
 
-let amount = parseInt(document.getElementById("amount").value)  
-let percent = parseInt(document.getElementById("percent").value)/100 
-let tipAmount = amount * percent
-let totalDue = amount + tipAmount
+
+
+let button = document.getElementById("button");
+button.addEventListener('click', function(e) {
+  //debugger  
+  //e.preventDefault()
+  let billAmount = document.getElementById("bill_amount")
+  let percent = parseInt(document.getElementById("percent").value)
+  let billAmountParsed = parseInt(billAmount.value)
+  
+  //let totalDue = (billAmountParsed + tipAmount)
+  //console.log('billAmountParsed:',billAmountParsed)
+  //console.log('percent', percent.toFixed(2))
+  let totalDue = calculateTip(billAmountParsed, percent)
+
+  displayResults(totalDue)
+}, false)
+
+function calculateTip(billAmountParsed, percent) {
+  let tipAmount = (billAmountParsed.toFixed(2)) * (percent/100)
+
+  console.log("tip amount is", tipAmount)
+  let totalDue = billAmountParsed + tipAmount
+  //console.log("total due:", totalDue.toFixed(2))
+  return totalDue
+}
+
+function displayResults(totalDue) {
+  console.log("total due:",totalDue.toFixed(2))
+  let total = document.getElementById("total")
+  total.innerHTML = totalDue.toFixed(2)
+}
